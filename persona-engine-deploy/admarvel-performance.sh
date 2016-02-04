815bb6ae554f048c52e2f10f32278c9c3b4b5aca
Timothy Atchison <tatchison@skyhookwireless.com>
2016-02-04 10:52:37 -0500

    fix typos
    fix typos

--- persona-engine-deploy/admarvel-performance.sh
+++ persona-engine-deploy/admarvel-performance.sh
@@ -45,19 +45,18 @@ do
 
 	echo $i
     	echo HOME/PERSONIFY START DATE FOR $i `date`
-	# nohup python ./home_personify_admarvel.sh -d $i -c admarveltest -s /var/lib/hadoop-hdfs/deploy/persona-engine-qa > home_personify_admarveltest.$i.out 2>&1
-	nohup python ./home_personify.py --cfg-file=admarvel_personify.properties --bind-date=$i > admarveltest_personification_$i.log 2>&1
-	hive -e "use admarveltest; select behavior_id, COUNT(behavior_id) mycount from user_behaviors group by behavior_id order by behavior_id;" > home_personify_admarveltest.$i.csv
+	nohup python ./home_personify.py --cfg-file=admarvel_personify.properties --bind-date=$i > admarveltest_personification_$i.out 2>&1
+	hive -e "use admarveltest; select behavior_id, COUNT(behavior_id) mycount from user_behaviors_internal group by behavior_id order by behavior_id;" > home_personify_admarveltest.$i.csv
     	echo HOME/PERSONIFY END DATE FOR $i `date`
 	diff home_personify_admarveltest.$i.csv ../baseline/home_personify_admarveltest.$i.csv > home_personify_admarveltest.$i.csv.diff
  
 	if [ ! -s home_personify_admarveltest.$i.csv.diff ]; then 
 		MAIL_TITLE='HOME PERSONIFY '$i' PASSED\!'
-		echo $MAIL_TITLE | cat home_personify_admarveltest.$i.out | mail -s '$MAIL_TITLE' tatchison@skyhookwireless.com
-		rm home_personify_admarveltest.$i.csv.diff home_personify_admarveltest.$i.csv home_personify_admarveltest.$i.out;
+		echo $MAIL_TITLE | cat admarveltest_personification_$i.out | mail -s '$MAIL_TITLE' tatchison@skyhookwireless.com
+		rm home_personify_admarveltest.$i.csv.diff home_personify_admarveltest.$i.csv admarveltest_personification_$i.out;
 	else 
 		MAIL_TITLE='HOME PERSONIFY '$i' FAILED\!'
-		echo $MAIL_TITLE | cat home_personify_admarveltest.$i.csv.diff home_personify_admarveltest.$i.out | mail -s '$MAIL_TITLE' tatchison@skyhookwireless.com
+		echo $MAIL_TITLE | cat home_personify_admarveltest.$i.csv.diff admarveltest_personification_$i.out | mail -s '$MAIL_TITLE' tatchison@skyhookwireless.com
 		exit 1;
 	fi
 
